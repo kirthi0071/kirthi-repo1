428b1bd350479da14c760c9f4a4692654e3591a2
kirthi0071 <kirthikumar077@gmail.com>
2025-09-17 17:07:53 +0530

    Update server.js

--- app2/server.js
+++ app2/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from App2!");
+  res.send("Hello from App2 root!");
 });
 
-// ✅ Same fix here
+app.get("/app2", (req, res) => {
+  res.send("Hello from App2 /app2 route!");
+});
+
 app.listen(5002, "0.0.0.0", () => {
   console.log("App2 running on port 5002");
 });
-
