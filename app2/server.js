845d63e4061c69e5935963f9d388e16b1d0205dc
kirthi0071 <kirthikumar077@gmail.com>
2025-09-17 16:51:15 +0530

    Update server.js

--- app2/server.js
+++ app2/server.js
@@ -1,4 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-app.get("/", (req, res) => res.send("Hello from Service 2 (Port 8080)!"));
-app.listen(8080, () => console.log("Service 2 running on port 8080"));
+
+app.get('/', (req, res) => {
+  res.send('Hello from App2!');
+});
+
+app.listen(5002, () => {
+  console.log('App2 running on port 5002');
+});
